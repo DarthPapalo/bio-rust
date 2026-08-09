e6db4aad499f2af3e4cef840c26b1b56f734f683
Pablo Vidal <darthpapalo@gmail.com>
2026-08-09 21:44:18 +0200

    Add K-mer utility methods to Sequence

--- src/sequence.rs
+++ src/sequence.rs
@@ -5,18 +5,37 @@ mod quality;
 
 use std::ops::{Deref, DerefMut};
 
+use bstr::{BStr, BString};
+
 pub use nucleotide::NucleotideView;
 pub use quality::{PhredQualityEncoding, QualityView};
 
-use bstr::{BStr, BString};
-
-#[derive(Debug, Default, PartialEq, Eq, Hash)]
+#[derive(Debug, Default, PartialEq, Eq, Hash, Clone)]
 pub struct Sequence(BString);
 
 /// Sequence creation functions
 impl Sequence {
     pub fn new(bytes: Vec<u8>) -> Self {
         Self(BString::new(bytes))
+    }
+}
+
+/// Sequence utility methods
+impl Sequence {
+    /// Returns a `slice::Windows` iterator over the sequence kmers of size `k`.
+    pub fn kmers(&self, k: usize) -> impl Iterator<Item = &BStr> {
+        self.0.windows(k).map(BStr::new)
+    }
+
+    /// Returns the number of appearances of `kmer` inside the sequence.
+    pub fn count_kmer<K>(&self, kmer: K) -> usize
+    where
+        K: AsRef<[u8]>,
+    {
+        let kmer = kmer.as_ref();
+        let kmers_iter = self.kmers(kmer.len());
+
+        kmers_iter.filter(|&k| k == kmer).count()
     }
 }
 
@@ -85,5 +104,30 @@ impl AsRef<BStr> for Sequence {
 impl From<Sequence> for BString {
     fn from(s: Sequence) -> Self {
         s.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_kmers() {
+        let kmers_vec: Vec<BString> = Sequence::from("CGTGAGTC")
+            .kmers(3)
+            .map(ToOwned::to_owned)
+            .collect();
+
+        assert_eq!(kmers_vec, vec!["CGT", "GTG", "TGA", "GAG", "AGT", "GTC"]);
+    }
+
+    #[test]
+    fn test_count_kmer() {
+        let seq = Sequence::from("AGTCTAGGATAGTTCGAG");
+
+        assert_eq!(seq.count_kmer("AG"), 4);
+        assert_eq!(seq.count_kmer("GT"), 2);
+
+        assert_eq!(seq.count_kmer("TAG"), 2);
     }
 }
